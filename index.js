fc0ac47f27fe0a7b52f11464db47655eef489fc0
wanglifa <wanglifa1995@gmail.com>
2019-03-23 20:51:38 +0800

    添加默认根目录下hello nodejs

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const cors = require('cors')
 //把用户传来的文件存到我服务器的uploads目录下，没有这个目录它会自动创建
 const upload = multer({dest: 'uploads/'})
 const app = express()
-
+app.get('/', (req, res) => {
+    res.send('hello nodejs')
+})
 //options和post都得加cors()
 app.options('/upload', cors())
 app.post('/upload', cors(), upload.single('file'),(req,res)=>{
